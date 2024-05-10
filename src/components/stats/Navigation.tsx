2fe8ac8b7d74176be81be7b66d0adb3887c1f908
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-05-10 15:33:00 +0500

    Give control for health progress

--- src/components/stats/Navigation.tsx
+++ src/components/stats/Navigation.tsx
@@ -1,48 +1,57 @@
-import healthIcon from "../../assets/icons/health.svg";
-import shieldIcon from "../../assets/icons/shield.svg";
-import energyIcon from "../../assets/icons/energy.svg";
 import React from "react";
-
-const icons = [
-  { icon: healthIcon, text: "Health" },
-  { icon: shieldIcon, text: "Shield" },
-  { icon: energyIcon, text: "Energy" },
-];
 
 interface NavIconProps {
   isActive: boolean;
   icon: {
     icon: string;
     text: string;
+    progress: number;
   };
 }
 const NavIcon: React.FC<NavIconProps> = ({ isActive, icon }) => {
-  const activeOuterStyles = isActive ? "border-white":"border-[#CACACA]"
-  const activeInnerStyles = isActive ? "bg-lightgreen":"custom-bg"
-  const activeInner2Styles = isActive ? "custom-shadow-active":"custom-shadow"
+  const activeOuterStyles = isActive ? "border-white" : "border-[#CACACA]";
+  const activeInner2Styles = isActive
+    ? "custom-shadow-active"
+    : "custom-shadow";
+
+  const customStyle = {
+    background: `linear-gradient(45deg, ${isActive ? "#4DFF74" : "#ffffff"} ${icon.progress}%, rgba(0,0,0,0.7) 0%)`,
+  };
+
   return (
-    <button className={`w-10 h-10 2xl:w-14 2xl:h-14 border bg-white bg-opacity-15 backdrop-blur-xl rounded-xl p-1 ${activeOuterStyles}`}>
-      <div className={`rounded-lg w-full h-full p-[3px] ${activeInnerStyles}`}>
-          <div className={`w-full h-full bg-[#616161] flex justify-center items-center rounded-md ${activeInner2Styles}`}>
-            <img src={icon.icon} alt="icon" className="w-4 h-4 2xl:w-6 2xl:h-6" />
-          </div>
+    <button
+      className={`w-10 h-10 2xl:w-14 2xl:h-14 border bg-white bg-opacity-15 backdrop-blur-xl rounded-xl p-1 ${activeOuterStyles}`}
+    >
+      <div className={`rounded-lg w-full h-full p-[3px]`} style={customStyle}>
+        <div
+          className={`w-full h-full bg-[#616161] flex justify-center items-center rounded-md ${activeInner2Styles}`}
+        >
+          <img src={icon.icon} alt="icon" className="w-4 h-4 2xl:w-6 2xl:h-6" />
+        </div>
       </div>
     </button>
   );
+};
+
+interface NavigationInterface {
+  icons: {
+    icon: string;
+    text: string;
+    progress: number;
+  }[];
 }
 
-const Navigation: React.FC = () => {
+const Navigation: React.FC<NavigationInterface> = ({ icons }) => {
   return (
     <div className="flex gap-1 2xl:gap-2">
       {icons.map((icon, index) => (
-        <NavIcon key={index} icon={icon} isActive={index===0}/>
+        <NavIcon key={index} icon={icon} isActive={index === 0} />
       ))}
     </div>
   );
 };
 
 export default Navigation;
-
 
 // REMOVED CODE: (KEEPING IT IN CASE WE NEED IT LATER)
 // import foodIcon from "../../assets/icons/food.svg";
--- src/components/stats/main.tsx
+++ src/components/stats/main.tsx
@@ -1,13 +1,22 @@
 import Navigation from "./Navigation"
+import healthIcon from "../../assets/icons/health.svg";
+import shieldIcon from "../../assets/icons/shield.svg";
+import energyIcon from "../../assets/icons/energy.svg";
 // import Bar from "./Bar";
 
+
+const icons = [
+  { icon: healthIcon, text: "Health", progress: 100 },
+  { icon: shieldIcon, text: "Shield", progress: 75 },
+  { icon: energyIcon, text: "Energy", progress: 25 },
+];
 
 const Stats = () => {
   
   return (
     <div className="fixed bottom-5 left-5 space-y-2">
       {/* <Bar progress={10*active+(Math.random()*50)} /> */}
-      <Navigation />
+      <Navigation icons={icons} />
     </div>
   )
 }
